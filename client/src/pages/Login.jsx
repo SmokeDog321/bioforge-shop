918e5cf0af5f8301e360248d119eb6e1a350dbf9
SmokeDog321 <kychkinll@gmail.com>
2026-07-22 23:06:27 +0900

    Update password placeholder

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -46,7 +46,7 @@ export default function Login() {
               type="password"
               value={password}
               onChange={e => setPassword(e.target.value)}
-              placeholder="••••••••"
+              placeholder="Пароль"
               required
             />
           </div>
